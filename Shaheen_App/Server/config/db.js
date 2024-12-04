77998cbdc9679cfb6cfe32934ae16ff237dbbe5a
Areej Omar Abumuhfouz <122300349+progAreej@users.noreply.github.com>
2024-12-04 13:25:31 +0300

    Update db.js

--- Shaheen_App/Server/config/db.js
+++ Shaheen_App/Server/config/db.js
@@ -5,8 +5,8 @@ require('dotenv').config(); // To load environment variables
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     });
 
     console.log('MongoDB connected successfully');
